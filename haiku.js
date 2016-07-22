7ee41261eed42b49f626f1be4ad1c49ebe282693
BearGrappler <changingtimes@gmail.com>
2016-07-22 09:59:27 -0500

    Got Haiku to work. Working on Extra-Credit

--- haiku.js
+++ haiku.js
@@ -3,37 +3,76 @@
 // console.log("Node starting...");
 
 var fs = require('fs');
-var cmudictFile = readCmudictFile('./truncatedCmudict.txt');
+//truncated was so that I could look at output more easily
+var cmudictFile = readTxtFile('./Cmudict.txt');
 
-function readCmudictFile(file){
+var macBethFile = readTxtFile('./macBethFile.txt')
+
+// console.log(cmudictFile);
+
+
+formatData([[5],[7],[5]], cmudictFile);
+
+
+function readTxtFile(file){
 	return fs.readFileSync(file).toString();
 }
+// DEBUGGING STARTS
 // console.log(fs);
 // console.log(fs.readFileSync('./cmudict.txt'));
+// DEBUGGING ENDS
 
-function formatData(data){
+function formatData(structureArr, data){
 	var lines = data.toString().split("\n"), //lines -> array of strings where each line is an array entry
-		lineSplit,
-		wordToSyllableDictionary = {};
+		lineSplit, 
+		syllableCount,
+		wordsArray = [],
+		syllablesArray,
+		wordToSyllableDictionary = {},
+		syllableToWordDictionary = {};  //{'1': array of all the words with 1 syllable, "2":}
 
 
-	//as you push words in use objects for key-value mapping for number of syllables in a word better than an array
-	//especially if you have a lot of words.
+	// as you push words in use objects for key-value mapping for number of syllables in a word better than an array
+	// especially if you have a lot of words.
 	// console.log(line[10].split(" "));
 
 
-
+	// console.log(lines);
 	lines.forEach(function(line){
 		lineSplit = line.split(" ");
-		wordToSyllableDictionary[lineSplit[0]] = countSyllables(lineSplit);
-		console.log("The word " + lineSplit[0] + " has this phoneme    layout: " + lineSplit[1]);
+		wordsArray.push(lineSplit[0]);
+		syllableCount = countSyllables(lineSplit);
+		wordToSyllableDictionary[lineSplit[0]] = syllableCount;
+
+		if(syllableToWordDictionary[syllableCount.toString()] === undefined){
+			syllableToWordDictionary[syllableCount.toString()] = [];
+			syllableToWordDictionary[syllableCount.toString()].push(lineSplit[0]);
+		}
+		else{
+			syllableToWordDictionary[syllableCount.toString()].push(lineSplit[0]);
+		}
+
+		// console.log("The word " + lineSplit[0] + " has this phoneme    layout: " + lineSplit[1]);
 	})
 
-	console.log(wordToSyllableDictionary);
+	// console.log(syllableToWordDictionary);
+	// console.log(lineSplit);
+
+
+
+	syllablesArray = generateSyllablesArray(syllableToWordDictionary, structureArr);
+
+	// console.log(syllablesArray);
+	// console.log([wordToSyllableDictionary,syllablesArray]);
+	// console.log(wordsArray);
+	return  [wordToSyllableDictionary,syllablesArray]
 }
 
-function countSyllables(strArr){  //counting syllables for each word
-	var syllablesArr = strArr.slice(2),  //assuming the format is like ["wholeWord","", //actual syllables]
+
+
+
+function countSyllables(wordArr){  //counting syllables for each word
+	var syllablesArr = wordArr.slice(2),  //assuming the format is like ["wholeWord","", //actual syllables]
 	numberOfSyllables = 0;
 
 	for(var i = 0; i < syllablesArr.length; i++){
@@ -42,23 +81,74 @@ function countSyllables(strArr){  //counting syllables for each word
 			numberOfSyllables++;
 		}
 	}
+	// console.log(wordArr);
+	// console.log(syllablesArr);
 
 	return numberOfSyllables;
 
 }
 
-formatData(cmudictFile);
 
-function createHaiku(structure){
-	console.log("this should log a haiku with the structure " + structure);
+//will use this to create arrOfWords
+function generateSyllablesArray(arg1, structureArr){
+	//arg1 is syllabletoWordDictionary
+	//the structureArr contains the word count per line
 
+	var output =[],
+	totalNumbOfWordsNeeded = 0, //could make this more efficient
+	maxNumbOfSyllablesInOneWord=7;  // will need to change this
 
+	// console.log(arg1);
+	for(var i = 0; i < structureArr.length; i++){
+		totalNumbOfWordsNeeded += structureArr[i].length;
+	}
+
+	// console.log("We are entering j loop!")
+	for(var j = 0; j <= maxNumbOfSyllablesInOneWord; j++){
+		output.push([]);
+		for(var k= 1; k<= totalNumbOfWordsNeeded; k++){
+			var arrayOfWordsForThisNumbOfSyllables = arg1[j.toString()];
+			// console.log(arrayOfWordsForThisNumbOfSyllables);
+			var randomIntForPullingARandomWord = Math.floor(Math.random()*arrayOfWordsForThisNumbOfSyllables.length);
+			output[j].push(arrayOfWordsForThisNumbOfSyllables[randomIntForPullingARandomWord]);
+			//do this until you hit total Numb of words neede;
+		}
+		// console.log("We exited k loop!");
+	}
+	// console.log("We exited");
+	return output; 
+
+}
+
+//the array of words index corresponds to number of syllables in that array
+function createHaiku(structure, syllablesArr){
+ 
+	// console.log("this should log a haiku with the structure " + structure);
+	// console.log('sylls', syllablesArr);
+	var arrOfWords;
+  	return structure.map(function(lines){
+    	return lines.map(function(syls){
+    		console.log('syls: ', syls)
+      		arrOfWords = syllablesArr[syls];
+      		return arrOfWords[Math.floor(Math.random() * arrOfWords.length)];
+    	}).join(' ');
+	}).join('\n');
+
+}
+
+function searchForHaikuStructure(structure, wordTosyllableDictionary, textBeingAnalyzed){
+	
 }
 
 module.exports = {
+	readTxtFile: readTxtFile,
+	formatData: formatData,
+	countSyllables: countSyllables,
 	createHaiku: createHaiku,
-}
 
+}
+//Learning experience it didn't work because we didn't export the functions since
+//it's a module
 
 // console.log(module);
 
--- haikuGenerator.js
+++ haikuGenerator.js
@@ -1,7 +1,24 @@
 // console.log(module);
 
+
 var haiku = require('./haiku');
 
-var creation = haiku.createHaiku("Random structure");
+var cmudictFile = haiku.readTxtFile('./Cmudict.txt');
+// var structure = [[5],[7],[5]]; //this works
+
+var structure = [ [2,3],
+				  [1,3,3],
+				  [3,2],
+		    	];
+
+var intermediate = haiku.formatData(structure, cmudictFile);
+console.log("We exited intermediate")
+
+// console.log(intermediate[1])
+
+var creation = haiku.createHaiku(structure, intermediate[1]);
 
 console.log(creation);
+
+//do not forget to change numbOfWordsCap and maxNumbOfSyllablesPossible 
+// in the haiku file
